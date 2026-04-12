97d04ab51ab900735585b3b992031c9c618b4bee
JeongYeonJun <jyj36900@gmail.com>
2026-04-12 22:26:16 +0900

    chore: fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'), // 경로 유지
         secure: false,
       },
     },
